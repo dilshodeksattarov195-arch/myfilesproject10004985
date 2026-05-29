ddec1032282fddf48eeee025b1035bb566844021
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-29 19:45:52 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class sessionFerifyWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = sessionFerifyWorker()
+    print("Worker engine initialized.")
